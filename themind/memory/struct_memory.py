41f1c46cb4a0f1efbea6b253a4d121f14f8e3ba8
David Mokos <davidmokos@gmail.com>
2023-12-05 13:41:10 -0800

    MVP of struct memory

--- themind/memory/struct_memory.py
+++ themind/memory/struct_memory.py
@@ -1,6 +1,7 @@
 import os
 import json
-import pydantic
+from jsonpath_ng import parse
+from genson import SchemaBuilder
 
 class StructuredMemory:
 
@@ -9,7 +10,7 @@ class StructuredMemory:
         self.id = id
         self.memory = {}
         self.schema = ""
-        self._load_memory(id)
+        self._load_memory()
 
 
     def _load_memory(self):
@@ -17,12 +18,6 @@ class StructuredMemory:
         if os.path.exists(file_path):
             with open(file_path, 'r') as f:
                 self.memory = json.load(f)
-        else:
-            with open(file_path, 'w') as f:
-                json.dump({}, f)
-
-
-    
 
     
     def _memory_file_path(self):
@@ -30,3 +25,71 @@ class StructuredMemory:
         file_path = os.path.join(base_dir, "data", f"{self.id}.json")
 
         return file_path
+    
+
+    def _save_memory(self):
+        with open(self._memory_file_path(), 'w') as f:
+            json.dump(self.memory, f, indent=4)
+
+    
+    def query(self, jsonPath: str) -> list:
+        jsonpath_expr = parse(jsonPath)
+        matches = [match.value for match in jsonpath_expr.find(self.memory)]
+
+        return matches
+    
+    def add_value(self, path: str, value: any):
+        keys = path.split('.')
+        temp = self.memory
+
+        for key in keys[:-1]:
+            temp = temp.setdefault(key, {})
+
+        temp[keys[-1]] = value
+
+        self._save_memory()
+
+    def append_to_list(self, path: str, value: any):
+        keys = path.split('.')
+        temp = self.memory
+
+        for key in keys[:-1]:
+            temp = temp.setdefault(key, {})
+
+        if keys[-1] in temp:
+            if isinstance(temp[keys[-1]], list):
+                temp[keys[-1]].append(value)
+            else:
+                raise ValueError(f"Value at {path} is not a list.")
+        else:
+            temp[keys[-1]] = [value]
+
+        self._save_memory()
+
+    def generate_schema(self):
+        builder = SchemaBuilder()
+        builder.add_object(self.memory)
+        schema = builder.to_schema()
+        self._remove_required(schema)
+
+        return json.dumps(schema, indent=4)
+
+        # return schema
+
+    def _remove_required(self, schema):
+        schema.pop('required', None)
+        for value in schema.get('properties', {}).values():
+            if 'properties' in value:
+                self._remove_required(value)
+            if 'items' in value:
+                self._remove_required(value['items'])
+
+
+
+if __name__ == "__main__":
+    memory = StructuredMemory("1")
+    res = memory.query("name")
+    memory.append_to_list("test_list", {"location": "Golden Gate", "time": "12:00"})
+    
+    print(memory.memory)
+    print(memory.generate_schema())
