2007e82edc2e772fb678a04e574e05ee96bf57b9
Adam Zvada <zvadaada@gmail.com>
2023-12-13 15:48:57 -0800

    readme and stuff

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# themind-memory
-TheMind Memory Architecture
+<h2 align="center">
+  FluidDB
+</h2>
+<h3 align="center">
+  Self-Organizing Databases
+</h3>
+
+---
+> Research repo for experimenting with the concept of a fluid database.
+
+![llm-os](./assets/FluidDB.png)
+
+### What is Fluid Database?
+
+Fluid database is a concept for databases that can update their schema based on the ingested data.
+
+Key features:
+- Adaptive Schema: Automatically adjusts its structure based on the data being ingested.
+- Flexible Querying: Supports both traditional query languages and intuitive natural language queries.
+- Versatile Data Input: Enables data entry in natural language, as well as in semi-structured formats.
+
+#### FluidDB Example
+
+FluidDB saves unstructured data in a structured form.
+```py
+# Saving a simple natural language statement into FluidDB
+fluiddb.save(uid, "hey adam here and i like thinking about the roman empire")
+```
+FluidDB automatically updates its schema to reflect this new data:
+```json
+{
+    "name": "Adam",
+    "interests": ["thinking about the Roman Empire"]
+}
+```
+
+Further, FluidDB demonstrates its versatility with semi-structured data formats. For instance:
+
+```py
+# Storing structured data like an email
+email = {
+    "from": "david@themind.ai",
+    "to": "adam@themind.ai",
+    "subject": "Check this out",
+    "content": "Hi Adam, Hope this email finds you well ..."
+}
+fluiddb.save(uid, email)
+```
+FluidDB then incorporates this new information into its schema:
+```json
+{
+    "name": "Adam",
+    "interests": ["thinking about the Roman Empire", "blue bottle"],
+    "email": [{
+        "from": "david@themind.ai",
+        "to": "adam@themind.ai",
+        "subject": "Check this out",
+        "content": "Hi Adam, Hope this email finds you well ..."
+    }]
+}
+```
+
+### Getting Started
+
+```bash
+building...
+```
--- themind/agents/chat_agent.py
+++ themind/agents/chat_agent.py
@@ -3,32 +3,40 @@ from themind.schema.message import Message
 from themind.schema.location import Location
 from themind.llm.openai_llm import OpenAILLM
 from themind.agents.struct_memory_agent import StructMemoryAgent
-from themind.database.firestore.thread_repository import ThreadRepository
+from themind.services.thread_service import ThreadService
 
 
 class ChatAgent(object):
 
     def __init__(self, firebase_app):
         self.llm = OpenAILLM()
-        self.thread_repo = ThreadRepository(firebase_app)
 
         self.struct_memory_agent = StructMemoryAgent()
+        
+        self.thread_service = ThreadService(firebase_app)
+        
+        # setup for function calling
+        self.functions = [self.update_memory_function]
+        self.functions_map = {}
+        for function in self.functions:
+            self.functions_map[function.name] = function
 
     def chat(self, uid: str, content: str, thread_id: str = None, location: Location = None):
 
         # get the thread from the database
-        thread = self.get_thread(uid=uid, thread_id=thread_id)
-        
+        thread = self.thread_service.get_thread(uid=uid, thread_id=thread_id)
         # append the message to the thread
         thread.add_message(message=Message.user_message(content=content))
-        
+
+        # run rag on memory (we could use router which decides if to use strctrured memory or unstracted)
+
         # run maybe retrive from agent memory
         self.struct_memory_agent.retrieve_struct_memory(uid=uid, thread=thread)
     
         # ASYNC[run  maybe save to memory with retrived info]
         self.struct_memory_agent.update_struct_memory(uid=uid, thread=thread)
         
-        self.update_thread(uid=uid, thread=thread)
+        self.thread_service.update_thread(uid=uid, thread=thread)
 
         def on_complete_closure(response):
             self.save_reponse(uid, thread_id, response)
@@ -47,17 +55,10 @@ class ChatAgent(object):
             
         return thread
     
-    # TODO: move to a ThreadService
-    def update_thread(self, uid: str, thread: Thread):
-        # TODO: 
-        self.thread_repo.update_thread(uid=uid, thread=thread)
-    
-    
-    # TODO: move to a ThreadService
     def save_reponse(self, uid: str, thread_id: str, reply: str):
-        thread = self.thread_repo.get_thread(uid=uid, thread_id=thread_id)
+        thread = self.thread_service.get_thread(uid=uid, thread_id=thread_id)
         thread.add_message(message=Message.assistent_message(content=reply))
-        self.thread_repo.update_thread(uid=uid, thread=thread)
+        self.thread_service.update_thread(uid=uid, thread=thread)
 
 
 if __name__ == '__main__':
