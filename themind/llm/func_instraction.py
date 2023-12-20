165614bd36a3c3394a0329e7c6c4c629152a36d9
Adam Zvada <zvadaada@gmail.com>
2023-12-20 15:28:42 +0100

    question and answear chunking strategy

--- themind/llm/func_instraction.py
+++ themind/llm/func_instraction.py
@@ -1,6 +1,7 @@
 from inspect import signature
 from pydantic.main import create_model
 from themind.llm.openai_llm import OpenAILLM
+from pydantic import BaseModel
 
 
 def instruct(func):
@@ -13,20 +14,26 @@ def instruct(func):
         # Get the function signature
         sig = signature(func)
 
-        # Create a Pydantic model from the return annotation
-        return_annotation = sig.return_annotation
-        ResponseModel = create_model('ResponseModel', result=(return_annotation, ...))
-        
-        # Bind the function arguments to their values
+        # Format the docstring with the arguments
         bound_args = sig.bind(*args, **kwargs)
         bound_args.apply_defaults()
-
-        # Format the docstring with the arguments
         formatted_docstring = docstring.format_map(bound_args.arguments)
 
+        # Create a Pydantic model from the return annotation
+        return_annotation = sig.return_annotation
+
+        if isinstance(return_annotation, type) and issubclass(return_annotation, BaseModel):
+            ResponseModel = sig.return_annotation
+        else:
+            ResponseModel = create_model('ResponseModel', result=(return_annotation, ...))
+        
         # run the OI call with reponse model
         openai_llm = OpenAILLM()
         result = openai_llm.instruction_instructor(formatted_docstring, response_model=ResponseModel)
+
+        # return the result if return value if raw type
+        if result.__class__.__name__ == 'ResponseModel':
+            return result.result
 
         return result
 
--- themind/vectorstores/vectorstore.py
+++ themind/vectorstores/vectorstore.py
@@ -4,10 +4,17 @@ from langchain.vectorstores import Chroma
 from langchain_core.documents import Document
 from dotenv import load_dotenv
 from themind.llm.func_instraction import instruct
+from pydantic import BaseModel
 
 
 # Load OI api key for the embeddings
 load_dotenv()
+
+
+class QAModel(BaseModel):
+    reasoning: List[str]
+    question: List[str]
+    answers: List[str]
 
 
 class VectorStore(object):
@@ -15,25 +22,42 @@ class VectorStore(object):
     def __init__(self, local_storage_dir: str = "./"):
         self.vectorstore = Chroma(collection_name="all-data", persist_directory=local_storage_dir, embedding_function=OpenAIEmbeddings())
     
-    def ingest(self, uid: str, data: List[str]):
-        
-        # for each chunk, crete a list a question which explain the chunk itself, similar how embeddings are being trained
+    def ingest(self, uid: str, data: List[str], chunking_strategy: str = "questions"):
+
+        # Question & Answear strategy
+        # for each chunk, crete a list a question and answear from the text, similar how embeddings are being trained!
 
         @instruct
-        def find_question(text: str) -> List[str]:
-            "Give me all the questions that give answears to this text: {text}"
+        def find_question(text: str) -> QAModel:
+            """
+            Give me all the questions that can be answered based on the TEXT below.
+            Use only the information you have in the TEXT. Don't add anything else.
             
+            If you generate the question reply, use the same tone of voice as the TEXT.
+
+            Here is the TEXT: 
+            {text}
+            """
+
+        chunks = []
         for chunk in data:
-            question = find_question(chunk) 
+            question = find_question(chunk)
+
+            print('Chunk: ' + chunk)
             print(question)
-        
-        
-        chunks = [
-            Document(page_content=s, metadata={"uid": uid})
-            for i, s in enumerate(data)
-        ]
-    
-        self.vectorstore.add_documents(chunks)
+
+            if len(question.question) != len(question.answers):
+                raise ValueError("The number of questions and answers do not match.")
+            
+            for q, a in zip(question.question, question.answers):
+                content = f"{q}\n{a}"
+                print('Content: ')
+                print(content)
+                doc = Document(page_content=content, metadata={"uid": uid})
+                chunks.append(doc)
+
+            print('Added chunk to vectorstore')
+            self.vectorstore.add_documents(chunks)
 
     
     def query(self, uid: str, query: str):
@@ -41,8 +65,7 @@ class VectorStore(object):
         
         return output
     
-    
-    
+
 if __name__ == '__main__':
     
     uid = 'test'
